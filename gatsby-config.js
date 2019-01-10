62d418118699e4e6e91f245a1811c6f2f52902d7
Douglas Matoso <doug2k1@gmail.com>
2019-01-10 13:14:00 -0200

    Fix config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'A starter blog demonstrating what Gatsby can do.',
     siteUrl: 'https://webdevdrops.com',
   },
-  pathPrefix: '',
+  pathPrefix: '/',
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
